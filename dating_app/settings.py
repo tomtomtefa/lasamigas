6a774e816ecce3d9185ea0f6a1a605c0bcd6bb51
Tom <tonemail@gmail.com>
2025-03-14 12:30:14 +0100

    Mise à jour de l'URL Redis pour Railway

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -58,7 +58,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.getenv("REDIS_URL")],
+            "hosts": [os.getenv("REDIS_URL", "redis://default:gWRytHCkmHMSTcTJhDgnjtsemDTscQCU@redis.railway.internal:6379")],
         },
     },
 }
